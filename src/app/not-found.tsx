49c4d0ee8e55c62621d228e8bde87cb0ae1e015e
André devkcud Albanese Junior <patommmmm@proton.me>
2025-05-15 16:04:50 -0300

    feat: Add minimal not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+import { Icon } from '@iconify/react';
+import Link from 'next/link';
+
+export default function NotFound() {
+    return (
+        <section className="w-full min-h-screen flex flex-col justify-center items-center gap-4">
+            <Icon
+                icon="garden:face-very-sad-fill-12"
+                className="text-red-500 text-9xl"
+            />
+
+            <div className="text-center">
+                <h2 className="text-xl text-red-500 font-bold uppercase">
+                    Página Não Encontrada
+                </h2>
+                <p>Não encontramos esta página.</p>
+            </div>
+
+            <Link
+                href="/"
+                className="m-4 bg-green-800 text-white px-4 py-2 rounded-xl flex gap-2 items-center"
+            >
+                <Icon icon="mdi:home" />
+                Voltar a home
+            </Link>
+        </section>
+    );
+}
