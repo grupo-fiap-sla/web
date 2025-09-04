1d7929ba4cd61b05a688654dd86923bdfe0f74eb
gobsz <gabimmoreira14@gmail.com>
2025-09-04 14:34:35 -0300

    prettier format

--- src/app/(landing)/weather-check/helpers.ts
+++ src/app/(landing)/weather-check/helpers.ts
@@ -1,7 +1,8 @@
 // Vetor com Dados de Horas Seguintes
 export const MOCK_HOURLY_CODES = [
-    10, 11, 11, 12,12, 12, 13, 45, 61, 33, 32, 31, 30, 30, 21, 22, 22, 32, 23, 22, 31, 31, 10, 10,
-]; 
+    10, 11, 11, 12, 12, 12, 13, 45, 61, 33, 32, 31, 30, 30, 21, 22, 22, 32, 23,
+    22, 31, 31, 10, 10,
+];
 
 // Vetor com Dados de Dias Seguintes
 export const MOCK_DAILY_CODES = [3, 80, 61, 1, 0, 2, 95];
